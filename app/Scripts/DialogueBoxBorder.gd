95639c5056c19ee5d1108253b3dc50bcc21ad8d4
ailurada <132323791+ailurada@users.noreply.github.com>
2024-04-06 15:48:37 -0700

    stretchy dialogue box

--- app/Scripts/DialogueBoxBorder.gd
+++ app/Scripts/DialogueBoxBorder.gd
@@ -1,16 +1,39 @@
 extends RichTextLabel
 
-var top_border = "================================================================================"
+var char_width_px = 20
+var num_rows = 5
+
+var top_border = "=="
 var space = ""
+
+func generate_border(width_px):
+
+	top_border = "=="
+	space = ""
+	
+	var new_border = ""
+	
+	for i in range(width_px/char_width_px - 2):
+		top_border += "="
+		space += " "
+	
+	new_border = top_border
+	
+	for i in range(num_rows):
+		new_border += "\n|" + space + "|"	
+	
+	new_border += "\n" + top_border
+
+	self.text = new_border
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for i in range(len(top_border) -2):
-		space += " "
-		
-	self.text = top_border + "\n|" + space + "|\n|" + space + "!\n" + top_border
-
+	var win_width = get_viewport().size.x
+	generate_border(win_width)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	var win_width = get_viewport().size.x
+	generate_border(win_width)

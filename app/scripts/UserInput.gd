3b6bfb1a5ba5e0be4ace905608d6b52c5e05516f
Bryan Silva <bryanjsilva04@gmail.com>
2024-04-06 13:32:18 -0700

    RichTextLabel echoes back what you type in TextEdit

--- app/scripts/UserInput.gd
+++ app/scripts/UserInput.gd
@@ -1,16 +1,24 @@
 extends TextEdit
 
 
+
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
-
+export var text_output_path: NodePath
+var text_output
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	text_output = get_node(text_output_path)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.scancode == KEY_ENTER:
+			text_output.text += self.text + "\n> "
+			self.text = ""

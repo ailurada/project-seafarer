cff3876e20b6a5af90064683183f47f350c759e1
Bryan Silva <bryanjsilva04@gmail.com>
2024-04-06 14:43:58 -0700

    Added Jetbrains Mono font

--- app/scripts/UserInput.gd
+++ app/scripts/UserInput.gd
@@ -11,10 +11,17 @@ var start_position: Vector2
 
 export var line_width: int
 
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	text_output = get_node(text_output_path)
 	start_position = self.rect_position
+	
+	#var dynamic_font = DynamicFont.new()
+	#dynamic_font.font_data = load("res://Fonts/JetBrainsMono-Regular.ttf")
+	#dynamic_font.size = 12
+	#self.set("custom_fonts/font", dynamic_font)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.

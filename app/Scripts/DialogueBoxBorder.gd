21d2b4ec5fd35bba3b3b719dc391cd2f9bc8e5bd
ailurada <132323791+ailurada@users.noreply.github.com>
2024-04-06 17:49:34 -0700

    centered box

--- app/Scripts/DialogueBoxBorder.gd
+++ app/Scripts/DialogueBoxBorder.gd
@@ -1,7 +1,8 @@
-extends RichTextLabel
+extends Label
 
 var char_width_px = 20
 var num_rows = 5
+var pt_size = 30
 
 var top_border = "=="
 var space = ""
@@ -23,17 +24,34 @@ func generate_border(width_px):
 		new_border += "\n|" + space + "|"	
 	
 	new_border += "\n" + top_border
-
+	
 	self.text = new_border
+	
+func update_size():
+	self.rect_size.x = len(top_border)*char_width_px
+	self.rect_size.y = len(self.text)/len(top_border)*pt_size + 80
 
+# put dialogue box at bottom
+func update_position(win_width, win_height):
+	self.rect_position.x = (win_width - self.rect_size.x)/2
+	self.rect_position.y = win_height - self.rect_size.y - pt_size - 15
+	
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	var win_width = get_viewport().size.x
 	generate_border(win_width)
+	self.align = Label.ALIGN_CENTER
 	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var win_width = get_viewport().size.x
+	var viewport_size = get_viewport().size
+	var win_width = viewport_size.x
+	var win_height = viewport_size.y
+	
 	generate_border(win_width)
+	update_size()
+	update_position(win_width, win_height)
+
+
--- app/Scripts/DialogueBoxText.gd
+++ app/Scripts/DialogueBoxText.gd
@@ -2,16 +2,19 @@ extends RichTextLabel
 
 var chars_per_update = 1
 
+
+# sets text to be displayed in the dialogue box
+func set_text(text):
+	self.text = text
+
+func update_size():
+	var border_size = get_parent().rect_size
+	self.rect_size.x = border_size.x - 10
+	self.rect_size.y = border_size.y - 10
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	self.text  = "something or other. some more stuff and stuff"
-	
-	# center in border 
-	self.anchor_left = 0.5
-	self.anchor_right = 0.5
-	self.anchor_top = 0.5
-	self.anchor_bottom = 0.5
-	
 	
 	# start with text not visible
 	self.visible_characters = 0;
@@ -19,8 +22,16 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	self.visible_characters += chars_per_update
-
-# sets text to be displayed in the dialogue box
-func set_text(text):
-	self.text = text
+	update_size()
+	self.set_position(get_parent().rect_position)
+	print(get_parent().rect_position)
+	self.anchor_top = 0.5
+	self.anchor_bottom = 0.5
+	self.anchor_left = 0.5
+	self.anchor_right = 0.5
+	
+	self.margin_top = -self.rect_size.y / 2
+	self.margin_bottom = -self.rect_size.y / 2
+	self.margin_left = -self.rect_size.x / 2
+	self.margin_right = -self.rect_size.x / 2
 	

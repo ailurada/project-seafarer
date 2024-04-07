958c90c46ce97192bbdafc0e6ec42adfc3ec8752
EightBitByte <apixelatedpug@gmail.com>
2024-04-06 18:09:03 -0700

    :sparkles: Node & JSONs completed

--- app/json.cs
+++ app/json.cs
@@ -9,6 +9,7 @@ public class json : Node
 		File file = new File();
 		file.Open(filePath, File.ModeFlags.Read);
 		string rawJSON = file.GetAsText();
+		file.Close();
 
 		JSONParseResult jsonResult = JSON.Parse(rawJSON);
 		var dict = jsonResult.Result as Godot.Collections.Dictionary;
@@ -16,11 +17,53 @@ public class json : Node
 		return dict;
 	}
 
+	SeaNode[] LoadSeaNodes(string filePath, out int amount) {
+		Godot.Collections.Dictionary dict = readJSON(filePath);
+		int amtNodes = dict.Count;
+		amount = amtNodes;
+		SeaNode[] nodeList = new SeaNode[amtNodes];
+
+		// For each node in the JSON file, translate to a SeaNode object and store in the array.
+		for (int i = 0; i < amtNodes; ++i) {
+			Godot.Collections.Dictionary nodeInfo = dict[i.ToString()] as Godot.Collections.Dictionary;
+
+			// Translate each adjacency list into digestible form
+			var adjArray = nodeInfo["adjacent"] as Godot.Collections.Array;
+
+			int adjCount = adjArray.Count;
+			int j = 0;
+
+			int[] adjList = new int[adjCount + 1];
+
+			for (; j < adjCount; ++j)
+				adjList[j] = Int32.Parse((string)adjArray[j]);
+
+			adjList[j] = -1;
+
+			SeaNode newNode = new SeaNode(
+				i,
+				Int32.Parse((string)nodeInfo["eid"]),
+				(string)nodeInfo["title"],
+				(string)nodeInfo["description"],
+				adjList,
+				(bool)nodeInfo["visited"]
+			);
+
+			nodeList[i] = newNode;
+		}
+
+		return nodeList;
+	}
+
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var dict = readJSON("res://data/test.json");
-			
-		GD.Print(dict["status"]);
+		int numNodes = 0;
+		var nodes = LoadSeaNodes("res://data/nodes.json", out numNodes);
+
+		for (int i = 0; i < numNodes; ++i) {
+			nodes[i].printNode();
+		}
+
 	}
 }

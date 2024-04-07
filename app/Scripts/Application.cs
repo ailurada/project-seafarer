b7a3d00a0524bdfbe1a7cfaccb9d22930a9d786a
Joonhyuk Song <john.j.song@hotmail.com>
2024-04-06 17:37:07 -0700

    Added basic framework and some implementation for the application class, which handles the game logic.

---
+++ app/Scripts/Application.cs
@@ -0,0 +1,144 @@
+using System;
+using System.Text;
+
+
+
+public enum State {
+	WAIT_CHOICE_NODE,
+	WAIT_CHOICE_EVENT
+}
+
+class Application
+{
+public Application() {}
+public ~Application() {}
+
+public Initialize() {
+	// call files...
+	// Init Nodes
+	// Init Events
+	// read the map!
+}
+
+public void UserInput(int input) {
+	if (m_state == WAIT_CHOICE_NODE) {
+		NodeChoiceHandler(input - 1);
+	}
+	else if (m_state == WAIT_CHOICE_EVENT) {
+		EventChoiceHandler(input - 1);
+	}
+}
+
+private void NodeChoiceHandler(int choice) {
+	if (choice < 0 || choice > m_adjacencyList.GetLength(1)) {
+		return;
+	}
+
+	if (choice < m_adjacencyList.GetLength(1)) {
+		TravelEdge(m_adjacencyList[m_nodeId, choice]);
+	}
+}
+
+private void TravelEdge(int destination) {
+	m_nodeId = destination;
+
+	// using System // needed for random
+	Random random = new Random(); // move to init
+	int randomNumber = random.Next(0, 19);
+
+	// mapping data structure (array) during event init
+	// EventId fired = ...;
+
+	
+	// DrawEvent(m_events[fired].GetTitle(), m_events[fired].GetDescription(), m_events[fired].GetChoiceDescriptions());
+	m_state = WAIT_CHOICE_EVENT;
+	m_eventId = fired;
+	return;
+}
+
+private EventChoiceHandler(int choice) {
+	if (choice < 0 || choice > m_events[m_eventId].NumChoices()) {
+		return;
+	}
+
+	EventId dest = (m_events[m_eventId].GetChoiceDestinations())[choice];
+
+	if (dest == -1) {
+		m_state = WAIT_CHOICE_NODE;
+		return;
+	}
+	else {
+		// DrawEvent(m_events[fired].GetTitle(), m_events[fired].GetDescription(), m_events[fired].GetChoiceDescriptions());
+		m_state = WAIT_CHOICE_EVENT;
+		m_eventId = dest;
+	}
+}
+
+private PrintMap() {
+	// should know grid size, somehow...
+	const int gridHeight = 15;
+	const int gridWidth = 41;
+	// should know map size, somehow, too....
+	const int mapHeight = 140;
+	const int mapWidth = 400;
+	
+	int centerRow = m_nodes[m_nodeId].GetRow();
+	int centerCol = m_nodes[m_nodeId].GetCol();
+	
+	int top = centerRow - (gridHeight - 1) / 2;
+	int bottom = centerRow + (gridHeight - 1) / 2;
+	if (top < 0) {
+		top = 0;
+		bottom = gridHeight - 1;
+	}
+	else if (bottom >= mapHeight) {
+		top = mapHeight - gridHeight - 2;
+		bottom = mapHeight - 1;
+	}
+
+	int left = centerCol - (gridWidth - 1) / 2;
+	int right = centerCol + (gridWidth - 1) / 2;
+	if (left < 0) {
+		left = 0;
+		right = gridWidth - 1;
+	}
+	else if (right >= mapWidth) {
+		left = mapWidth - gridWidth - 2;
+		right = gridWidth - 1;
+	}
+	
+	// construct the map as a string
+	StringBuilder sb = new StringBuilder();
+	
+	for (int row = top; row < bottom; ++row) {
+		for (int col = left; col < right; ++col) {
+			sb.Append(m_map[row, col]);
+		}
+		sb.Append("\n");
+	}
+	
+	// DrawMap(sb.ToString());
+}
+
+
+private char[,] m_map = null; // INIT BY READING MAP FILE
+
+private State m_state = WAIT_CHOICE_NODE;
+private Node[] m_nodes = null;
+private NodeId[,] m_adjacencyList = null;
+private Event[] m_events = null;
+
+// USER STATE
+private NodeId m_nodeId;
+private EventId m_eventId;
+
+private int m_health;
+private int m_gold;
+
+
+}
+
+
+
+
+
